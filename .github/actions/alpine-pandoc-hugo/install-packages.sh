0319915f7f9c8712ab70ffe0fae30d4ebaec416c
Carsten Gips <cagix@fh-bielefeld.de>
2022-02-10 11:42:56 +0100

    add Github action and workflow to build slides

---
+++ .github/actions/alpine-pandoc-hugo/install-packages.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+## Update TeX Live Manager
+tlmgr update --self || exit 1
+
+## Install extra packages for Beamer/Metropolis
+tlmgr install beamertheme-metropolis pgfopts tcolorbox environ || exit 1
+
+## Install extra packages for LaTeX conversion to .png
+tlmgr install standalone filemod currfile || exit 1
+
+## Install make and graphviz (dot)
+apk --no-cache add make bash graphviz ghostscript font-noto || exit 1
+
+## Install current Hugo
+wget https://github.com/gohugoio/hugo/releases/download/v0.92.1/hugo_0.92.1_Linux-64bit.tar.gz && tar -zxf hugo_*.tar.gz -C /usr/bin/ && rm hugo_*.tar.gz  || exit 1
